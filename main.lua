66e43091f48e110c2231554a5981583cc9bba22e
Casry <84832193+NotCasry@users.noreply.github.com>
2023-03-19 19:24:44 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,4 @@
+--https://xheptcofficial.gitbook.io/kavo-library/
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("WWRE Hub", "DarkTheme")
